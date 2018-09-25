07a1cb6a507ea52dc3b0ce1d26d367c8f44771b9
dragon788 <github@deik.me>
2018-09-25 11:54:11 -0500

    Update bats-test to use Docker container

--- bats-test.sh
+++ bats-test.sh
@@ -2,4 +2,4 @@
 # Don't use `-r test` because the test/libs directory has MANY tests from the bats* submodules
 if [ $# -gt 0 ]; then ARGS="$@"; else ARGS="test/*.bats"; fi
 # Run this file to run all the tests, once
-./test/libs/bats/bin/bats $ARGS
+docker run -it --rm -v "$(pwd):/opt/bats" --workdir /opt/bats bats/bats:latest $ARGS
